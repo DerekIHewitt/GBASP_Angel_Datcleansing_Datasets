b044242300e333ca9d05202a45d56d1b86faeee8
DerekIHewitt <78212549+DerekIHewitt@users.noreply.github.com>
2023-04-18 08:08:07 +0100

    Mid April Commit

--- Tables/dbo.SC_DATA_ROANNE_GBCDW.sql
+++ Tables/dbo.SC_DATA_ROANNE_GBCDW.sql
@@ -1,15 +1,15 @@
 CREATE TABLE [dbo].[SC_DATA_ROANNE_GBCDW]
 (
-[MIG_Object] [bigint] NULL,
-[MIG_Part] [int] NULL,
-[MIG_Customer] [int] NULL,
-[MIG_Line_Date] [datetime] NULL,
-[MIG_NISP_Type] [varchar] (7) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[MIG_Price] [decimal] (28, 0) NULL,
-[MIG_IP_Freq] [decimal] (28, 0) NULL,
-[MIG_SER_Freq] [decimal] (28, 0) NULL,
+[MIG_Object] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MIG_Part] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MIG_Customer] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MIG_Line_Date] [datetime] NOT NULL,
+[MIG_NISP_Type] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MIG_Price] [decimal] (18, 2) NOT NULL,
+[MIG_IP_Freq] [decimal] (18, 2) NOT NULL,
+[MIG_SER_Freq] [decimal] (18, 2) NULL,
 [MIG_SER_Date] [datetime] NULL,
-[PO_NUMBER] [varchar] (31) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[PO_NUMBER] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [PO_EXPIRY_DATE] [datetime] NULL
 ) ON [PRIMARY]
 GO
